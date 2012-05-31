9f5cd7247a1e8362bfb1ae2ac6d9d2efbf5f1020
Frederik Reifschneider <reifschneider@karazy.net>
2012-05-31 11:42:28 +0200

    added a test class for registration controller

---
+++ test/unit/registrationSpec.js
@@ -0,0 +1,18 @@
+/** Unit testing registration controller. */
+
+describe("Cloobster.Registration", function {
+	var ctrl;
+
+	//setup necessary functions and members
+	beforeEach(function(){ 
+
+	});
+
+	//test form reset
+	it("should clear form on cancel call", function() {
+
+	});
+
+
+
+});
