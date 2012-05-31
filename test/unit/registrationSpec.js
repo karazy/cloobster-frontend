683ad483c73c0e4acdc7e358ebfee8a2a24e866c
Frederik Reifschneider <reifschneider@karazy.net>
2012-05-31 11:59:56 +0200

    test enhanced

--- test/unit/registrationSpec.js
+++ test/unit/registrationSpec.js
@@ -1,15 +1,35 @@
+'use strict';
 /** Unit testing registration controller. */
 
 describe("Cloobster.Registration", function {
-	var ctrl;
+	var ctrl,
+		scope,
+		account;
 
 	//setup necessary functions and members
 	beforeEach(function(){ 
+		scope = {
+			account: {
+				name : 'Hans Dampf'
+			}
+		};
+		module('Cloobster.services');
+        inject(function($injector) {
+            loginServiceMock = $injector.get('login');
+            facebookApiMock = $injector.get('facebookApi');
+            accountMock = $injector.get('Account');
+        });
 
+		//$scope, $resource, $location, Account, facebookApi, $routeParams, loginService
+		ctrl = new Cloobster.Registration(scope, $resource, $location, accountMock, facebookApiMock, $routeParams, loginServiceMock);
 	});
 
 	//test form reset
 	it("should clear form on cancel call", function() {
+
+		scope.cancel();
+
+		expect(scope.account.name).toEqual("");
 
 	});
 
