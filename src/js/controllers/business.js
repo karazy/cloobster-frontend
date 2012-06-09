c650e3e8f5a63394c7f289680621be76bd58fce8
Frederik Reifschneider <reifschneider@karazy.net>
2012-06-09 17:20:20 +0200

    add business form implemented, it is now possible to add businesses and to edit them

--- src/js/controllers/business.js
+++ src/js/controllers/business.js
@@ -10,12 +10,14 @@
 * 	View and manage businesses such as restaurants.
 * 	@constructor
 */
-Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadService, Business, $log) {
+Cloobster.Business = function($scope, $http, $routeParams, $location, loginService, uploadService, Business, $log) {
 
 		/** Holds the Id of the active modal dialog. */
 	var activeModalDialog = "",
-		/** Resource used for the currently edited image. */
-		imageResource;
+		/** Template resource used to create concrete image resources. */
+		imageResource,
+		/** Active image resource. */
+		activeImage;
 
 	/** Resource for CRUD on businesses. */	
 	$scope.businessResource = null;
@@ -26,20 +28,12 @@ Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadS
 	$scope.activeProperty = null;
 	/** When true user can edit the business profile. */
 	$scope.editMode = false;
+	/** True if file upload is active */
 	$scope.activeFileUpload = null;
-
-	function checkNested(obj, properties) {
-		  var args = properties.split('.');
-	      // obj = args.shift();
-
-	  for (var i = 0; i < args.length; i++) {
-	    if (!obj.hasOwnProperty(args[i]) && obj[args[i]]) {
-	      return false;
-	    }
-	    obj = obj[args[i]];
-	  }
-	  return true;
-	}
+	/** Contains the information about a new business. */
+	$scope.newBusiness = null;
+	/** When true shows the form for a new business. */
+	$scope.showNewBusinessForm = false;
 
 	/**
 	* Returns all businesses
@@ -76,13 +70,55 @@ Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadS
 	*/
 	$scope.loadBusiness = function(id) {
 		$scope.activeBusiness = $scope.businessResource.get({'id' : id}, function() {
-			imageResource =	Business.buildImageResource($scope.activeBusiness.id);
+			imageResource =	createImageResource($scope.activeBusiness.id);
 			//if no images are included init with empty object
 			$scope.activeBusiness.images = $scope.activeBusiness.images || {};
 
 			$scope.activeFileUpload = uploadService.getFileUploadObject('fileModal', imageResource);
 		});
 	};
+
+	/**
+	* Show/hide new business form.
+	*/
+	$scope.toggleNewBusiness = function() {
+		$scope.showNewBusinessForm = !$scope.showNewBusinessForm;
+	};
+
+	/**
+	* Adds a new business.
+	* @param businessName
+	*	Name for the new business
+	*/
+	$scope.addNewBusiness = function() {
+
+		if($scope.newBusinessForm.$valid) {
+			$scope.newBusinessEntity = new $scope.businessResource({
+			'name' : $scope.newBusinessForm.name.$modelValue,
+			'city' : $scope.newBusinessForm.city.$modelValue,
+			'address' : $scope.newBusinessForm.address.$modelValue,
+			'postcode' : $scope.newBusinessForm.postcode.$modelValue,			
+			'phone' : $scope.newBusinessForm.phone.$modelValue,
+			'description' : $scope.newBusinessForm.description.$modelValue
+		});
+
+		$scope.newBusinessEntity.$save(function() {
+			//refresh business list upon success
+			$scope.loadBusinesses();
+		});
+
+		$scope.closeNewBusinessForm();
+		
+		}
+	};
+
+	/**
+	* Closes the new business form and resets values.
+	*/
+	$scope.closeNewBusinessForm = function() {
+		$scope.toggleNewBusiness();
+		$scope.newBusiness = {};
+	}
 
 	/**
 	* Switches between view and edit mode.
@@ -145,7 +181,7 @@ Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadS
 	*/
 	$scope.saveProperty = function() {
 		var property = $scope.activeProperty.property;
-		if(checkNested($scope.activeBusiness, property)) {
+		if($scope.activeBusiness.hasOwnProperty(property)) {
 			$scope.activeBusiness[property] = $scope.activeProperty.value;
 			$scope.activeBusiness.$update();
 
@@ -154,26 +190,92 @@ Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadS
 	};
 
 	/**
+	* Cancel editing property.
+	*/
+	$scope.cancelProperty = function() {
+		$scope.activeProperty = null;
+	};
+
+	/**
 	* Save last edited image in images property.
 	*/
 	$scope.saveImage = function() {
 		var property = $scope.activeProperty.property;
-		
-		$scope.activeBusiness.images[property] = {
-				url: imageResource.url,
-				blobKey: imageResource.blobkey
-		};
 
-		$scope.activeBusiness.$update();
+		
+		activeImage = new imageResource({
+    				id: property,
+    				blobKey: imageResource.blobKey,
+    				url: imageResource.url
+    	});
+
+		//save image under the given property. e. g. logo
+		imageResource.id = property;
+
+		activeImage.$save(function() {
+			//success callback
+			//set saved logo as new business logo
+			$scope.activeBusiness.images[property] = {
+				url: imageResource.url,
+				blobKey: imageResource.blobKey
+			};
+		});
+
 		$(activeModalDialog).modal('hide');
 	}
 
-	$scope.cancelProperty = function() {
-		$scope.activeProperty = null;
-	}
+	/**
+	* Discards changes made to an image and leaves edit mode.
+	*/
+	$scope.cancelImage = function() {
+		// $scope.logoFormMode = "view";
+		// $scope.logoUploadFinished = false;
 
-	/** Watches loggedIn status and initializes controller when status changes to true.
-	 *
+		//delete image from images bucket
+		if(activeImage && activeImage.blobKey) {
+			$http.delete('/uploads/images/' + activeImage.blobKey)
+			.success(function() {
+				activeImage = null;
+			})
+			.error(function() {
+				//handle error
+			});
+		}
+	};
+
+	/*
+	* Get css class for field highlighting.
+	* @returns error if dirty && invalid
+	*		  sucess if dirty && !invalid
+	*         empty string otherwise
+	*/
+	$scope.getFieldInputClass = function(dirty, invalid) {
+		if(dirty && invalid) {
+			return "error";
+		} else if (dirty && !invalid) {
+			return "success";
+		} else {
+			return "";
+		}
+	};
+
+
+
+	/**
+	* Creates an image resource. 
+	* @see Cloobster.services.Business#buildImageResource
+	*
+	* @param businessId
+	*	Id of business for which to create the resource
+	* @return
+	*	ImageResource
+	*/
+	function createImageResource(businessId) {
+		return Business.buildImageResource(businessId);
+	};
+
+	/** 
+	 * Watches loggedIn status and initializes controller when status changes to true.
 	 */
 	$scope.$watch('loggedIn', function(newValue, oldValue) {
 		if(newValue == true) {
@@ -183,12 +285,12 @@ Cloobster.Business = function($scope, $http, $routeParams, loginService, uploadS
 			if($routeParams && $routeParams.id) {
 				$scope.loadBusiness($routeParams.id);
 			}
-
-
+		} else if(newValue == false) {
+			$location.url('/');
 		}
 	});
 
 
 };
 
-Cloobster.Business.$inject = ['$scope', '$http','$routeParams', 'login', 'upload', 'Business', '$log'];
+Cloobster.Business.$inject = ['$scope', '$http','$routeParams', '$location', 'login', 'upload', 'Business', '$log'];
