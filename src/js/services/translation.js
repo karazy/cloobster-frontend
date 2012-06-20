135c56e1a67668029fe7572e0264cd086901871f
Frederik Reifschneider <reifschneider@karazy.net>
2012-06-20 15:18:13 +0200

    added translations for businesses partial

--- src/js/services/translation.js
+++ src/js/services/translation.js
@@ -10,11 +10,33 @@ angular.module("Cloobster.translations", [], ["$provide", function($provide) {
 	//holds all translations
 	var map = {
 		"DE" : {
+			//index
+			"product.brand" : "Cloobster",
+			"nav.logout" : "Logout",
+			"nav.login": "Login",
 			"nav.restaurants" : "Restaurants",
 			"nav.about" : "Über",
+			"nav.profile" : "Profil",
 			"nav.registration" : "Registrieren",
-			"business.title" : "Meine Restaurants",
-			"business.add" : "Restaurant hinzufügen"
+			//businesses partial
+			"businesses.title" : "Meine Restaurants",
+			"businesses.add" : "Restaurant hinzufügen",
+			"businesses.action.show" : "anzeigen/editieren",
+			"businesses.action.löschen" : "löschen",
+			"businesses.form.name" : "Name",
+			"businesses.form.description" : "Description",
+			"businesses.form.address" : "Address",
+			"businesses.form.city" : "Stadt",
+			"businesses.form.postcode" : "Postleitzahl",
+			"businesses.form.phone" : "Telefon",
+			"businesses.form.error.required" : "Bitte füllen Sie das Feld aus.",
+			"businesses.form.mandatory" : "Felder markiert mit * sind pflicht.",
+			//general
+			"common.warning.title" : "Warnung!",
+			"common.ok" : "Ok",
+			"common.cancel" : "Abbrechen",
+			"common.add" : "Hinzufügen",
+			"common.delete" : "Löschen"
 
 		},
 		"EN" : {
