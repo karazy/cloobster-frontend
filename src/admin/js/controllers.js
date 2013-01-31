86ebb852e29588a1b0c4cb305e1085f4c591c250
Nils Weiher <weiher@karazy.net>
2013-01-31 16:43:35 +0100

    admin/partials/main.html 	- Added area upgrade function to admin main view. 	- Removed "Create Dummie Accounts" button from admin main view. admin/js/functions.js 	- Moved CloobsterAdmin.Functions controller to this file. 	- Added function for master Spot upgrade. 	- Removed createDummieAccounts function from controller. admin/js/controllers.js 	- Moved CloobsterAdmin.Functions controller to own file

--- src/admin/js/controllers.js
+++ src/admin/js/controllers.js
@@ -97,7 +97,7 @@ CloobsterAdmin.Import.$inject = ['$scope', '$http', '$anchorScroll'];
 * 	@name CloobsterAdmin.Configuration
 *	@requires $http
 *
-* 	Manages data import.
+* 	Manages service configuration.
 * 	@constructor
 */
 CloobsterAdmin.Configuration = function($scope, $http, $anchorScroll, $timeout) {
@@ -244,84 +244,6 @@ CloobsterAdmin.PDFConfiguration = function($scope, $http, $timeout) {
 }
 CloobsterAdmin.PDFConfiguration.$inject = ['$scope', '$http', '$timeout'];
 
-
-CloobsterAdmin.Functions = function($scope, $http) {
-	$scope.deleteFunctionsDisabled = (Karazy.environment === "dev")? false : true;
-	$scope.confirmDeleteAllDisabled = false;
-	$scope.confirmDeleteLiveDisabled = false;
-
-	$scope.deleteAllData = function() {
-		$scope.confirmDeleteAllText = "Deleting ...";
-		$scope.confirmDeleteAllDisabled = true;
-		$http['delete']('/admin/s/datastore/all').success(function() {
-				$scope.confirmDeleteAllText = "All data deleted.";
-			}).error(function (data, status) {
-				$scope.confirmDeleteAllText = status + " error.";
-			});
-	};
-
-	$scope.deleteLiveData = function() {
-		$scope.confirmDeleteLiveText = "Deleting ...";
-		$scope.confirmDeleteLiveDisabled = true;
-		$http['delete']('/admin/s/datastore/live').success(function() {
-				$scope.confirmDeleteLiveText = "Live data deleted.";
-			}).error(function (data, status) {
-				$scope.confirmDeleteLiveText = status + " error.";
-			});
-	};
-
-	$scope.createDummieAccounts = function() {
-		$scope.createDummieAccountsText = "Creating ...";
-		$scope.createDummieAccountsDisabled = true;
-		$http.post('/admin/s/accounts/dummies', {}).success(function() {
-				$scope.createDummieAccountsText = "Accounts created.";
-			}).error(function (data, status) {
-				$scope.createDummieAccountsText = status + " error.";
-			});	
-	};
-
-	$scope.upgradeLocations = function() {
-		$scope.upgradeLocationsText = "Upgrading ...";
-		$scope.upgradeLocationsDisabled = true;
-		$http.put('/admin/s/dataupgrades/subscriptionandwelcome', {}).success(function(data) {
-				$scope.upgradeLocationsText = data['entityCount'] + " Locations updated.";
-			}).error(function (data, status) {
-				$scope.upgradeLocationsText = status + " error.";
-			});	
-	};
-
-	$scope.createTestData = function() {
-		$scope.createTestDataText = "Creating ...";
-		$scope.createTestDataDisabled = true;
-		$http.post('/admin/s/testdata', {}).success(function(data) {
-				$scope.createTestDataText = "Data created.";
-			}).error(function (data, status) {
-				$scope.createTestDataText = status + " error.";
-			});	
-	};
-
-	$scope.deleteTestData = function() {
-		$scope.deleteTestDataText = "Deleting ...";
-		$scope.deleteTestDataDisabled = true;
-		$http['delete']('/admin/s/testdata', {}).success(function(data) {
-				$scope.deleteTestDataText = "Data deleted.";
-			}).error(function (data, status) {
-				$scope.deleteTestDataText = status + " error.";
-			});	
-	};
-
-	$scope.sendCockpitUpdateMessage = function() {
-		$scope.sendCockpitUpdateMessageText = "Creating ...";
-		$scope.sendCockpitUpdateMessageDisabled = true;
-		$http.post('/admin/s/channels/messages', {'type':'application', 'action': 'update'}).success(function() {
-				$scope.sendCockpitUpdateMessageText = "Message sent.";
-			}).error(function (data, status) {
-				$scope.sendCockpitUpdateMessageText = status + " error.";
-			});	
-	};
-}
-
-CloobsterAdmin.Functions.$inject = ['$scope', '$http'];
 
 CloobsterAdmin.SelectBusiness = function($scope, $http) {
 	$scope.businessSelected = false;
