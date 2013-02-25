b65c595493dc061e96998a6cff84ad1aff7e91cb
Nils Weiher <weiher@karazy.net>
2013-02-25 10:53:06 +0100

    Added api-version header to frontend admin and home apps

--- src/home/js/app.js
+++ src/home/js/app.js
@@ -5,9 +5,12 @@ var Cloobster = Cloobster || {};
 
 // Declare app level module which depends on filters, and services
 Cloobster.customermodule = angular.module('CloobsterHome', ['Cloobster.services', 'Cloobster.directives']).
-  config(['$routeProvider','configProvider', function($routeProvider,configProvider) {
+  config(['$routeProvider','configProvider','$httpProvider', function($routeProvider,configProvider, $httpProvider) {
     // Here you set the service url that the Cloobster services use.
     //configProvider.setServiceUrl('https://eatsense-test.appspot.com');
+
+    // Set API Version header
+    $httpProvider.defaults.headers.common['cloobster-api'] = '2';
 
   	$routeProvider.when('/', {template: '...', controller: Cloobster.Redirect});
     $routeProvider.when('/confirm/:emailToken', {templateUrl: 'partials/confirmemail.html', controller: Cloobster.ConfirmAccount});
