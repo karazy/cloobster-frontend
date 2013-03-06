63361182618a6440886a6fc4966562be029b9839
Nils Weiher <weiher@karazy.net>
2013-03-06 11:42:09 +0100

    [completed:519] Fixed Bug with infopage data mixup, effectively overriding the previous selected info page in the browser after saving.

--- src/frontend/js/controllers/infopage.js
+++ src/frontend/js/controllers/infopage.js
@@ -134,6 +134,7 @@ Cloobster.InfoPage = function($scope, $http, $routeParams, $location, loginServi
 
 		function saveSuccess(infopage) {
 			$scope.infopages.push(infopage);
+			$scope.selectedInfoPage = infopage;
 			$scope.imageResource = InfoPage.buildImageResource(activeBusinessId, infopage.id);
 		}
 	}
