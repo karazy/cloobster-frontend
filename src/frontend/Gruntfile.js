80118862993c1e0f275cdb02ac0408399d80967c
Frederik Reifschneider <frederik.reifschneider@gmail.com>
2014-08-13 23:05:19 +0200

    added cloobster logo instead of referencing from home folder; changed livereload settings in grunt

--- src/frontend/Gruntfile.js
+++ src/frontend/Gruntfile.js
@@ -14,10 +14,10 @@ module.exports = function(grunt) {
 		},
 		connect: {
 	      options: {
-	        port: 9000,
+	        port: 9001,
 	        // Change this to '0.0.0.0' to access the server from outside.
 	        hostname: '0.0.0.0',
-	        livereload: 35729
+	        livereload: 35730
 	      },
 	      livereload: {
 	        options: {
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
 		        	// 'copy:whitelabelSrc'
 		        ],
 		        options: {
-		          livereload: true,
+		          livereload: 35730,
 		          //spawn no child processes, needed to access options
 		          spawn: false
 		        }
