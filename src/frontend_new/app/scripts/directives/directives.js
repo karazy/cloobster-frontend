612c9d578ea90332df67b552fc16a495bf4c4378
Frederik Reifschneider <frederik.reifschneider@gmail.com>
2014-11-28 07:18:42 +0100

    moved tooltip into custom file

--- src/frontend_new/app/scripts/directives/directives.js
+++ src/frontend_new/app/scripts/directives/directives.js
@@ -61,35 +61,6 @@ Cloobster.directives.directive('l', ['$locale', 'lang', '$interpolate', function
 }]);
 
 /**
-* Used to display a Bootstrap tooltip which gets localized.
-* http://twitter.github.com/bootstrap/javascript.html#tooltips
-*/
-Cloobster.directives.directive('tooltip', ['$locale', 'lang', function($locale, langService) {
-	return function(scope, iElement, iAttrs, controller) {
-		var key = iAttrs.tooltip,
-			//position of the tooltip
-			position = iAttrs.tooltipPosition || "top",
-			translation;
-
-		if(!key) {
-			return;
-		}
-
-		//if no translation is found, don't replace html, this is useful to provide default values in html
-		translation = langService.translate(key) || key;
-
-		if(translation) {
-			iElement.tooltip({
-				"title" : translation,
-				"placement" : position,
-				"trigger" : "hover focus"
-			});
-		}
-
-	};
-}]);
-
-/**
 *
 */
 Cloobster.directives.directive('showRole', ['login', function(login) {
